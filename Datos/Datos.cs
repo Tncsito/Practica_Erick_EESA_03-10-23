79394e9a7456e93866fb34f14389618a37be8aae
LDVP5049 <luismaster752@gmail.com>
2023-10-03 13:06:27 -0700

    Agregar archivos de proyecto.

---
+++ Datos/Datos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Mia.Datos
+{
+    public class Datos
+    {
+        /*######### EJERCICIO 1 ################*/
+        public int E1()
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, };
+        }
+
+
+
+
+
+
+
+
+        /*######### EJERCICIO 2 ################*/
+
+        /*######### EJERCICIO 3 ################*/
+
+        /*######### EJERCICIO 4 ################*/
+
+        /*######### EJERCICIO 5 ################*/
+
+    }
+}
